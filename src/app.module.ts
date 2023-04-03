507a65a1e39eaf7125c3f09a4282fef856a225fc
Mario Espasa <mario.espasa.2003@gmail.com>
2023-04-03 11:45:47 +0200

    Variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
+//npm i --save @nestjs/config
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot('')],
+  imports: [ProductsModule, ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
